f80de6cbed3f816b2eee6f0452bb3e210bcca97c
IsmaelPSY <ismaelpsy.12@gmail.com>
2020-08-12 18:03:45 -0500

    feat: remaking front-end

--- front-pizza/src/App.js
+++ front-pizza/src/App.js
@@ -1,36 +1,24 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Tab, Row, Col, Nav } from 'react-bootstrap';
+import logo from './logo.svg';
 import './App.css';
-import Home from './components/HomeComponent/Home';
 
 function App() {
   return (
-    <div className="container-fluid">
-        <Tab.Container id="left-tabs-pizza" defaultActiveKey="first" className="h-100">
-          <Row>
-            <Col sm={3}>
-              <Nav variant="pills" className="flex-column">
-                <Nav.Item>
-                  <Nav.Link eventKey="first">Inicio</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Col>
-            <Col sm={9}>
-              <Tab.Content className="h-100">
-                <Tab.Pane eventKey="first">
-                  <Home />
-                </Tab.Pane>
-                <Tab.Pane eventKey="second">
-                Contenido delTab2
-                </Tab.Pane>
-              </Tab.Content>
-            </Col>
-          </Row>
-        </Tab.Container>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
